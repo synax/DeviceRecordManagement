6f2480278d127fb87efab377a3d4a04e4deb3c49
Stefan Henseler <stefan.henseler@synax.ch>
2013-03-17 19:08:31 +0100

    Delete Device By Name Added

--- DeviceRecordManagement/DeviceRecordManagement.js
+++ DeviceRecordManagement/DeviceRecordManagement.js
@@ -12,39 +12,114 @@
 $(function() {
 
 		// Set Single Source Policy => Allow Cross Domain Requests
-			$.support.cors = true;		
 			
-			$("#deleteButton").button()
+			$.support.cors = true;
+			
+			$.SCCMSettings = { 
+				WebserviceUri : "http://zrheae18.emea.kuoni.int/deploymentwebservice/sccm.asmx", 
+				SiteCode : "ZRH" 
+			};
+			
+			$("#deleteButtonMac").button();
+			
+			$("#deleteButtonName").button();
+			
+			
+			$("#accordion").accordion();
+					
+			var cache = {};
+			
+			$("#resourceSelect").autocomplete({
+			  delay: 1000,
+  			  select: function( event, ui ) {
+			  
+				$("#resourceID").text(ui.item.value);
+				$("#resourceSelect").val(ui.item.label);
+				$("#deleteButtonName").button("enable");
+				$("#contentWidgetComputerName").addClass("ui-state-highlight");	
+				return false;
+			  
+			  },
+			  minLength: 2,
+			  source: function( request, res ) {
+				var term = request.term;
+								
+				var resources =  new Array();        
+			
+                $.ajax({
+			        type: "GET",
+					async: true,
+			        url:  $.SCCMSettings.WebserviceUri + "/SearchComputerByName",
+			        contentType: "text/xml; charset=utf-8",
+			        data: {SearchString:term,siteCode:$.SCCMSettings.SiteCode},
+			        success: function (response) {
+					
+					
+						
+			            $(response).find("Resource").each(function () {			                     
+			              
+							resources.push( { "label":$(this).find("NetbiosName").text() , "value":$(this).find("ResourceID").text()  } );					
+			
+             
+			            });
+						
+						
+						res( resources );
+					
+			        },
+			        error: function (response) {
+			            res(resources);			       
+			        }
+			    });	
+				
+				
+				
+				
+				 
+				}
+			  
+			});
+			
+			
 			
 			$("#macAddress").change(function() { 
 				
-				if (IsDeviceKnown()) {
-				
-					var DeviceName = GetDeviceNameByMac();
-				
-					if (DeviceName == "" || DeviceName == "error") {
-					
-						$("#deviceName").text("Device not found...");			
+				//if (IsDeviceKnown() == "true") {				
 						
-					} 	
-					else {
-						$("#deviceName").text(DeviceName);
-						$("#deleteButton").button("enable");
-					}
-
+					var DeviceName = GetDeviceNameByMac();
 					
+					if (DeviceName == "error")  {
+						$("#deviceName").text("Webservice request error...");
+						$("#contentWidgetMacAddress").addClass("ui-state-error");	
+						
+					} else {
+				
+						if (DeviceName == "" ) {
+						
+							$("#deviceName").text("Device name not found...");	
+							$("#contentWidgetMacAddress").addClass("ui-state-highlight");							
+							
+						} 	
+						else {
+							$("#deviceName").text(DeviceName);
+							$("#deleteButtonMac").button("enable");
+							$("#contentWidgetMacAddress").addClass("ui-state-highlight");	
+						}
 					
-				} else {
+					}					
+					
+				//} else {
 				
-					$("#deviceName").text("Device not found...");
+					//$("#deviceName").text("Device is not known in SCCM...");
 				
-				}			
+				//}			
 				
 			});
+				
 			
 		
-			$("#deleteButton").click(function() {
-				var result = DeleteMachine();
+			$("#deleteButtonMac").click(function() {
+				var result = DeleteMachineByMac();
 				
 				if (result == "false" || result == "error") {
 					
@@ -56,8 +131,34 @@ $(function() {
 					
 					$("#removalResult").text("Device '" + $("#deviceName").text() + "' (MAC: " + $("#macAddress").val() + ") removed successfully!");	
 					$("#removalResult").addClass("ui-state-highlight");
-					$("#deleteButton").button("disable");
+					$("#deleteButtonMac").button("disable");
 					$("#macAddress").val("");
+					
+				}			
+				
+				$("#removalResult").show();	
+				
+			});
+			
+			
+			
+			
+			$("#deleteButtonName").click(function() {
+				var result = DeleteMachineByName();
+				
+				if (result == "false" || result == "error") {
+					
+					$("#removalResult").text("Device removal failed! Sure the client exists?");			
+					$("#removalResult").addClass("ui-state-error");		
+				} 		
+				
+				if (result == "true"){
+					
+					$("#removalResult").text("Device '" + $("#resourceSelect").val() + "' (Resource ID: " + $("#resourceID").text() + ") removed successfully!");	
+					$("#removalResult").addClass("ui-state-highlight");
+					$("#deleteButtonName").button("disable");
+					$("#resourceID").text("");
+					$("#resourceSelect").val("");
 					
 				}			
 				
@@ -66,7 +167,7 @@ $(function() {
 			});
 		
 		
-		 var DeleteMachine = function ()
+		 var DeleteMachineByMac = function ()
 		{
         
 				var result;
@@ -74,11 +175,10 @@ $(function() {
                 $.ajax({
 			        type: "GET",
 					async: false,
-			        url:  "http://selfservice.corp.viamonstra.com/deploymentwebservice/sccm.asmx/DeleteComputer",
+			        url:  $.SCCMSettings.WebserviceUri + "/DeleteComputer",
 			        contentType: "text/xml; charset=utf-8",
-			        data: {MACAddress:$("#macAddress").val(),UUID:"",SiteCode:"PS1"},
+			        data: {MACAddress:$("#macAddress").val(),UUID:"",SiteCode:$.SCCMSettings.SiteCode},
 			        success: function (response) {
-			            $('#result').html('success:');
 			            $(response).find("boolean").each(function () {			                     
 			              
 			               	result = $(this).text();			
@@ -97,6 +197,47 @@ $(function() {
 									
 			    
 		}
+		
+		var DeleteMachineByName = function ()
+		{
+        
+				var result;
+		
+                $.ajax({
+			        type: "GET",
+					async: false,
+			        url:  $.SCCMSettings.WebserviceUri + "/DeleteComputerByID",
+			        contentType: "text/xml; charset=utf-8",
+			        data: {ResourceID:$("#resourceID").text(),SiteCode:$.SCCMSettings.SiteCode},
+			        success: function (response) {
+			            $(response).find("boolean").each(function () {			                     
+			              
+			               	result = $(this).text();						
+							
+									             
+			            });
+			        },
+			        error: function (response) {
+			           result =  "error";			       
+			        }
+			    });	
+				
+				return result
+					
+									
+			    
+		}
+		
+		var GetResourceByName =function (term)
+		{
+		
+				
+				
+				return resources
+					
+									
+			    
+		}
 
 		var GetDeviceNameByMac = function ()
 		{
@@ -106,11 +247,11 @@ $(function() {
                 $.ajax({
 			        type: "GET",
 					async: false,
-			        url:  "http://selfservice.corp.viamonstra.com/deploymentwebservice/sccm.asmx/GetComputerName",
+			        url:  $.SCCMSettings.WebserviceUri + "/GetComputerName",
 			        contentType: "text/xml; charset=utf-8",
-			        data: {MACAddress:$("#macAddress").val(),UUID:"",SiteCode:"PS1"},
+			        data: {MACAddress:$("#macAddress").val(),UUID:"",SiteCode:$.SCCMSettings.SiteCode},
 			        success: function (response) {
-			            $('#result').html('success:');
+			          
 			            $(response).find("string").each(function () {			                     
 			              
 			               	result = $(this).text();			
@@ -138,11 +279,11 @@ $(function() {
                 $.ajax({
 			        type: "GET",
 					async: false,
-			        url:  "http://selfservice.corp.viamonstra.com/deploymentwebservice/sccm.asmx/IsComputerKnown",
+			        url:  $.SCCMSettings.WebserviceUri + "/IsComputerKnown",
 			        contentType: "text/xml; charset=utf-8",
-			        data: {MACAddress:$("#macAddress").val(),UUID:"",SiteCode:"PS1"},
+			        data: {MACAddress:$("#macAddress").val(),UUID:"",SiteCode:$.SCCMSettings.SiteCode},
 			        success: function (response) {
-			            $('#result').html('success:');
+			            
 			            $(response).find("boolean").each(function () {			                     
 			              
 			               	result = $(this).text();			
